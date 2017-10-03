4178c6c2dd72f47986d62abce0f527df98741023
Nikolay Postoyko <dotnicht@live.com>
2017-10-03 13:08:50 +0300

    adding biz assembly, renaming proj

---
+++ InvestorDashboard.Business/IConfigurationService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace InvestorDashboard.Business
+{
+    public interface IConfigurationService
+    {
+        string KeyVaultClientId { get; }
+        string KeyVaultClientSecret { get; }
+        Uri KeyValaultSecretUri { get; }
+    }
+}
