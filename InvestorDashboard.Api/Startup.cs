eaa6b8ef0e0ca14a90a9f985b4ee2704c2d54956
Nikolay Postoyko <dotnicht@live.com>
2017-11-20 18:47:45 +0200

    nlog

--- InvestorDashboard.Api/Startup.cs
+++ InvestorDashboard.Api/Startup.cs
@@ -15,6 +15,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using NLog.Web;
 using OpenIddict.Core;
 using OpenIddict.Models;
 using System;
