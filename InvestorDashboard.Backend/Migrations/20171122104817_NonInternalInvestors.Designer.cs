371ce1ba5c75b7a1de05cfae70c7eed86939c184
Denis Skvortsov <denis.skvortsow@outlook.com>
2017-11-22 12:48:55 +0200

    add migration

--- InvestorDashboard.Backend/Migrations/20171122104817_NonInternalInvestors.Designer.cs
+++ InvestorDashboard.Backend/Migrations/20171122104817_NonInternalInvestors.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace InvestorDashboard.Backend.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20171122104817_NonInternalInvestors")]
+    partial class NonInternalInvestors
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ InvestorDashboard.Backend/Migrations/20171122104817_NonInternalInvestors.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace InvestorDashboard.Backend.Migrations
+{
+    public partial class NonInternalInvestors : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TotalNonInternalInvestors",
+                table: "DashboardHistoryItems",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "TotalNonInternalUsdInvested",
+                table: "DashboardHistoryItems",
+                type: "decimal(18, 6)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TotalNonInternalInvestors",
+                table: "DashboardHistoryItems");
+
+            migrationBuilder.DropColumn(
+                name: "TotalNonInternalUsdInvested",
+                table: "DashboardHistoryItems");
+        }
+    }
+}
