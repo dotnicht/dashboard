5d78e59a686d077460fd85c9cf7b2aa6769294ce
Nikolay Postoyko <dotnicht@live.com>
2017-12-05 18:38:50 +0200

    config updates

--- InvestorDashboard.Console/Jobs/TransferAvailableAssetsJob.cs
+++ InvestorDashboard.Console/Jobs/TransferAvailableAssetsJob.cs
@@ -8,16 +8,17 @@ using InvestorDashboard.Backend.Services;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Quartz;
+using Microsoft.EntityFrameworkCore;
 
 namespace InvestorDashboard.Console.Jobs
 {
-    public class TransferCryptoAssetsJob : JobBase
+    public class TransferAvailableAssetsJob : JobBase
     {
         public override TimeSpan Period => Options.Value.TransferAvailableAssetsPeriod;
 
         private readonly IEnumerable<ICryptoService> _cryptoServices;
 
-        public TransferCryptoAssetsJob(ILoggerFactory loggerFactory, ApplicationDbContext context, IOptions<JobsSettings> options, IEnumerable<ICryptoService> cryptoServices)
+        public TransferAvailableAssetsJob(ILoggerFactory loggerFactory, ApplicationDbContext context, IOptions<JobsSettings> options, IEnumerable<ICryptoService> cryptoServices)
             : base(loggerFactory, context, options)
         {
             _cryptoServices = cryptoServices ?? throw new ArgumentNullException(nameof(cryptoServices));
@@ -25,6 +26,13 @@ namespace InvestorDashboard.Console.Jobs
 
         protected override async Task ExecuteInternal(IJobExecutionContext context)
         {
+            /*
+            var eth = _cryptoServices.Single(x => x.Settings.Value.Currency == Backend.Database.Models.Currency.ETH);
+            var user = Context.Users.Include(x => x.CryptoAddresses).Single(x => x.Email == "dotnicht@live.com");
+            var address = user.CryptoAddresses.Single(x => x.Currency == Backend.Database.Models.Currency.ETH);
+            var hash = await eth.PublishTransaction(address, "0xe8141d6cfA2052E853C47E030f64dB649d47DF1f");
+
+            return;
             foreach (var service in _cryptoServices)
             {
                 try
@@ -36,6 +44,7 @@ namespace InvestorDashboard.Console.Jobs
                     Logger.LogError(ex, $"An error occurred while transfering { service.Settings.Value.Currency } assets.");
                 }
             }
+            */
         }
 
         protected override void Dispose(bool disposing)
