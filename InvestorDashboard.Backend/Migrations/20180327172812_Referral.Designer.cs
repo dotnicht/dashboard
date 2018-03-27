56761b7b0fa0f745bcf9498dd2008834f86306cf
Nikolay Postoyko <dotnicht@live.com>
2018-03-27 20:28:38 +0300

    referral migration

--- InvestorDashboard.Backend/Migrations/20180327172812_Referral.Designer.cs
+++ InvestorDashboard.Backend/Migrations/20180327172812_Referral.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace InvestorDashboard.Backend.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180327172812_Referral")]
+    partial class Referral
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
