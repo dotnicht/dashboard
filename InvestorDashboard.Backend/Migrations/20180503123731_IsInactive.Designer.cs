d68117b99c064114456f4eac049fcd1c93f3a5b9
Nikolay Postoyko <dotnicht@live.com>
2018-05-03 15:37:43 +0300

    migration

--- InvestorDashboard.Backend/Migrations/20180503123731_IsInactive.Designer.cs
+++ InvestorDashboard.Backend/Migrations/20180503123731_IsInactive.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace InvestorDashboard.Backend.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180503123731_IsInactive")]
+    partial class IsInactive
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
