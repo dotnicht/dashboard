96068f5e69eab2f418437ef1ac13ee26d699651b
Nikolay Postoyko <dotnicht@live.com>
2018-05-28 17:19:39 +0300

    currency serialization

--- InvestorDashboard.Backend/Database/Models/Currency.cs
+++ InvestorDashboard.Backend/Database/Models/Currency.cs
@@ -1,9 +1,17 @@
-﻿namespace InvestorDashboard.Backend.Database.Models
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.Runtime.Serialization;
+
+namespace InvestorDashboard.Backend.Database.Models
 {
+    [JsonConverter(typeof(StringEnumConverter))]
     public enum Currency
     {
+        [EnumMember(Value = "USD")]
         USD = 0,
+        [EnumMember(Value = "ETH")]
         ETH = 1,
+        [EnumMember(Value = "BTC")]
         BTC = 2,
         Token = int.MaxValue
     }
