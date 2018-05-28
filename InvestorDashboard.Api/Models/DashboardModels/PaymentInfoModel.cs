3c43b01c224266b87c5a0d519709e95b2a9c3557
Nikolay Postoyko <dotnicht@live.com>
2018-05-28 17:15:17 +0300

    payment method update

--- InvestorDashboard.Api/Models/DashboardModels/PaymentInfoModel.cs
+++ InvestorDashboard.Api/Models/DashboardModels/PaymentInfoModel.cs
@@ -1,10 +1,13 @@
+using InvestorDashboard.Backend.Database.Models;
+
 namespace InvestorDashboard.Api.Models.DashboardModels
 {
     public class PaymentInfoModel
     {
-        public string Currency { get; set; }
+        public Currency Currency { get; set; }
         public decimal Rate { get; set; }
         public string Address { get; set; }
         public int Confirmations { get; set; }
+        public bool IsDisabled { get; set; }
     }
 }
