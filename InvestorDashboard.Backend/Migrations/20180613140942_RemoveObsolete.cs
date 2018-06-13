0413ed542b99e4d7eacb569a90f4dde43749e739
Nikolay Postoyko <dotnicht@live.com>
2018-06-13 17:28:42 +0300

    migrations

---
+++ InvestorDashboard.Backend/Migrations/20180613140942_RemoveObsolete.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace InvestorDashboard.Backend.Migrations
+{
+    public partial class RemoveObsolete : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsInvestor",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "KycBonus",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "TokensAvailableForTransfer",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "UseNewBonusSystem",
+                table: "AspNetUsers");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsInvestor",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<long>(
+                name: "KycBonus",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<long>(
+                name: "TokensAvailableForTransfer",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "UseNewBonusSystem",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
